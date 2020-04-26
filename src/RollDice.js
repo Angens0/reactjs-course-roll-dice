c72741f813b9d3e16e4e58c52a3329e66415799f
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-04-26 03:12:38 +0200

    54. Adding the RollDice Component

---
+++ src/RollDice.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import Die from './Die'
+
+class RollDice extends Component {
+    static defaultProps = {
+        sides: ['one', 'two', 'three', 'four', 'five', 'six']
+    }
+
+    randSide = () => {
+        const { sides } = this.props
+        return sides[Math.floor(Math.random() * sides.length)]
+    }
+
+    state = {
+        isRolling: false,
+        die1: this.randSide(),
+        die2: this.randSide()
+    }
+
+    startRolling = () => {
+        this.setState({
+            die1: this.randSide(),
+            die2: this.randSide(),
+            isRolling: true
+        })
+
+        setTimeout(this.stopRolling, 1000)
+    }
+
+    stopRolling = () => {
+        this.setState({ isRolling: false })
+    }
+
+    render() {
+        const { isRolling, die1, die2 } = this.state
+
+        return (
+            <div>
+                <Die face={die1} isRolling={isRolling} />
+                <Die face={die2} isRolling={isRolling} />
+                <button onClick={this.startRolling} disabled={isRolling}>Roll</button>
+            </div>
+        )
+    }
+}
+
+export default RollDice
